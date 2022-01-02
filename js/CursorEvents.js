1bd543a5797080a54e4c5fb078db379657b059d2
Mayank <70268410+Mayank300@users.noreply.github.com>
2022-01-02 15:52:29 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,47 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+
+  init: function () {
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvent();
+  },
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placeId = ["taj-mahal", "budapest", "eiffel-tower", "new-york-city"];
+    if (placeId.includes(id)) {
+      const placeHolderContainer = document.querySelector("#places-container");
+      placeHolderContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "red",
+        opacity: 1,
+      });
+    }
+  },
+
+  handleMouseEnterEvents: function () {
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+
+  handleMouseLeaveEvent: function () {
+    this.el.addEventListener("mouseleave", () => {
+      const { selectedItemId } = this.data;
+      if (selectedItemId) {
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if (id === selectedItemId) {
+          el.setAttribute("material", {
+            color: "black",
+            opacity: 1,
+          });
+        }
+      }
+    });
+  },
+});
